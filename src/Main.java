6ec2cdcff836fdfd122a897ad03aa38b6833d8e7
Regi Duquez <regiduquez@gmail.com>
2024-09-27 23:31:06 +0800

    added 2 new files

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+
+
 class Main{
     public static void main(String[] args){
         System.out.println("new project");
         System.out.println("second commit");
         System.out.println("third commit no staging");
+        System.out.println("git diff command");
     }
 }
