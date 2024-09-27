8752a9e498aec65367b02565f64cb7a84612021e
Regi Duquez <regiduquez@gmail.com>
2024-09-27 23:18:08 +0800

    third commit staging skipped

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args){
         System.out.println("new project");
         System.out.println("second commit");
+        System.out.println("third commit no staging");
     }
 }
