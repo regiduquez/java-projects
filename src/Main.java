cb0b6036b8719d9fa6e8ace2939ff311be3465a5
Regi Duquez <regiduquez@gmail.com>
2024-09-28 23:08:52 +0800

    Main.java updated

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,6 @@ class Main{
         System.out.println("third commit no staging");
         System.out.println("git diff command");
         System.out.println("git fetch origin main");
-        System.out.println("git pull will fetch from remote repo to local repo then merge local repo and working directory");
+        System.out.println("git pull will fetch from remote repo to local repo then merge local repo and working directoryq");
     }
 }
