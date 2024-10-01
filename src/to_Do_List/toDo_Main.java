ae49822a9ab051fa823850c06e9b4b7a37b8b3e9
Regi Duquez <regiduquez@gmail.com>
2024-10-02 00:53:15 +0800

    to do list switch case for user response initial

--- src/to_Do_List/toDo_Main.java
+++ src/to_Do_List/toDo_Main.java
@@ -1,5 +1,7 @@
 package to_Do_List;
 
+import com.sun.security.jgss.GSSUtil;
+
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,5 +14,33 @@ public class toDo_Main {
         System.out.println("add <task> | remove <task> | done <task> ");
         String[] response = scan.nextLine().split(" ", 2);
 
+        switch (response[0]){
+            case "add":
+                addTask();
+                break;
+            case "remove":
+                remTask();
+                break;
+            case "done":
+                doneTask();
+                break;
+            default:
+                System.out.println("we cannot understand you!");
+        }
+
+
+    }
+
+    static void addTask(){
+        System.out.println("Task added to list");
+
+    }
+    static void remTask(){
+        System.out.println("Task remove from list");
+
+    }
+
+    static void doneTask(){
+        System.out.println("Task successfully done!");
     }
 }
